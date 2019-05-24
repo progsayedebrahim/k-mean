e8abb1cddce54a162f76dade1cd7ce4d7848ab70
progsayedebrahim <38439102+progsayedebrahim@users.noreply.github.com>
2019-05-25 01:40:28 +0200

    Add files via upload
    the kmean dataset class is a class for initialize the dataset to be used in the algorithm

---
+++ kmeandatasetclass.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignment1;
+
+/**
+ *
+ * @author Sayed
+ */
+public class kmeandatasetclass {
+     
+    
+    private String Session_Connection_Time;
+    private String bytes_Transferred;
+    private String kali_Trace_Used;
+    private String servers_Corrupted;
+    private String pages_Corrupted;
+    private String location;
+    private String WPM_Typing_Speed;
+    
+    public String getSession_Connection_Time() {
+        return Session_Connection_Time;
+    }
+
+    public void setSession_Connection_Time(String Session_Connection_Time) {
+        this.Session_Connection_Time = Session_Connection_Time;
+    }
+
+    public String getBytes_Transferred() {
+        return bytes_Transferred;
+    }
+
+    public void setBytes_Transferred(String Bytes_Transferred) {
+        this.bytes_Transferred = Bytes_Transferred;
+    }
+
+    public String getKali_Trace_Used() {
+        return kali_Trace_Used;
+    }
+
+    public void setKali_Trace_Used(String Kali_Trace_Used) {
+        this.kali_Trace_Used = Kali_Trace_Used;
+    }
+
+    public String getServers_Corrupted() {
+        return servers_Corrupted;
+    }
+
+    public void setServers_Corrupted(String Servers_Corrupted) {
+        this.servers_Corrupted = Servers_Corrupted;
+    }
+
+    public String getPages_Corrupted() {
+        return pages_Corrupted;
+    }
+
+    public void setPages_Corrupted(String Pages_Corrupted) {
+        this.pages_Corrupted = Pages_Corrupted;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String Location) {
+        this.location = Location;
+    }
+
+    public String getWPM_Typing_Speed() {
+        return WPM_Typing_Speed;
+    }
+
+    public void setWPM_Typing_Speed(String WPM_Typing_Speed) {
+        this.WPM_Typing_Speed = WPM_Typing_Speed;
+    }
+    
+}
+
+
